189fe1cc0bb8b7c51b40216d68e64426383bb052
Gabriel Henrique <54925825+GabrielHLP@users.noreply.github.com>
2025-01-28 19:23:51 +0000

    adiciona script ´lint:check´ e ´lint:fix´

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home () {
-    return <h1>Este é a alteração de um commit</h1>
+function Home() {
+  return <h1>Este é a alteração de um commit</h1>;
 }
+
+function teste() {
+  console.log("teste");
+}
+
 
 export default Home;
